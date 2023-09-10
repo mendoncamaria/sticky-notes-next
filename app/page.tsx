a09bbaefb119e73a95640eba6762353ef10d39b7
Maria Mendonca <maria.mendonca156@gmail.com>
2023-09-10 20:24:28 +0530

    Fixed styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-'use client'
+'use client';
 import { useRef, useEffect, useState } from 'react';
 import Draggable from 'react-draggable';
 const { v4: uuidv4 } = require('uuid');
-var randomColor = require("randomcolor");
+var randomColor = require('randomcolor');
 
 import styles from './page.module.css';
 
@@ -15,9 +15,20 @@ type Item = {
 
 export default function Home() {
   const itemRef = useRef<HTMLInputElement>(null);
-  const [items, setItems] = useState<Item[]>(
-    JSON.parse(localStorage.getItem('items')) ?? []
-  );
+  const [items, setItems] = useState<Item[]>([]);
+  // (
+  //   JSON.parse(localStorage.getItem('items')) ?? []
+  // );
+  useEffect(() => {
+    // Check if localStorage is available (client-side)
+    if (typeof window !== 'undefined') {
+      const storedItemsString = localStorage.getItem('items');
+      const storedItems = storedItemsString
+        ? JSON.parse(storedItemsString)
+        : [];
+      setItems(storedItems);
+    }
+  }, []);
 
   const newitem = () => {
     if (itemRef.current && itemRef.current.value.trim() !== '') {
@@ -49,14 +60,16 @@ export default function Home() {
   };
 
   return (
-    <main className={styles.main}>
-      <input
-        ref={itemRef}
-        placeholder="Enter something..."
-        onKeyPress={(e) => {
-          if (e.key === 'Enter') newitem();
-        }}
-      />
+    <div className={styles.main}>
+      <div className={styles.inputWrapper}>
+        <input
+          ref={itemRef}
+          placeholder="Enter something..."
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') newitem();
+          }}
+        />
+      </div>
       <button onClick={newitem}>ENTER</button>
       {items.map((item: Item, index: number) => {
         return (
@@ -67,15 +80,18 @@ export default function Home() {
               updatePos(data, index);
             }}
           >
-            <div style={{ backgroundColor: item.color }} className="box">
+            <div style={{ backgroundColor: item.color }} className={styles.box}>
               {`${item.item}`}
-              <button id="delete" onClick={(e) => deleteNote(item.id)}>
+              <button
+                className={styles.delete}
+                onClick={(e) => deleteNote(item.id)}
+              >
                 X
               </button>
             </div>
           </Draggable>
         );
       })}
-    </main>
+    </div>
   );
 }
